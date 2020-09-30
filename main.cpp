d4f337db50633c0a6d114101b9dba161a1db926c
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2020-09-30 16:27:52 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,64 @@
+/*
+Bridget Naylor
+SID:025531413
+*/
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+//Question 1:
+// int returnBinaryBoundaryLine( int size, int list[] )
+// {
+//     int start = 0;
+//     int end = size;
+//     int middle = size / 2;
+//     bool foundK = false;
+//     while ( !foundK )
+//     {
+//         if ( (size == 1) || (list[ middle ] == 1 && list[ middle - 1 ] == 0) )
+//         {
+//             foundK = true;
+//             return middle;
+//         }
+//         else if ( list[ middle ] == 0 )
+//         {
+//             start = middle;
+//             middle += end - middle;
+//         }
+//         else if ( list[ middle ] == 1 )
+//         {
+//             end = middle;
+//             middle = (middle + start) / 2;
+//         }
+        
+//     }
+
+//     return 0;
+// }
+
+int main()
+{
+    //Question 1:
+    // int listSize;
+    // cout<<"Enter size of list: "<<endl;
+    // cin>>listSize;
+    // int listBinary[listSize];
+    // cout<<"input: a = ";
+    // for ( int i = 0; i < listSize; i++)
+    // {
+    //     cin>>listBinary[i];
+    //     if ( listBinary[i] != 0 && listBinary[i] != 1 )
+    //     {
+    //         cout<<"Non binary numbers detected. Try again"<<endl;
+    //     }
+    // }
+    
+    // cout<<"output: K = "<<returnBinaryBoundaryLine( listSize, listBinary )<<endl;
+    
+    
+    //Question 2:
+    
+    
+    return 0;
+}
+
